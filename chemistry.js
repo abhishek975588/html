77db225db45ee741a9b9b965c424bde58430e632
abhishek975588 <bhargavaa013@gmail.com>
2025-02-25 13:27:48 +0530

    second day changes

---
+++ chemistry.js
@@ -0,0 +1,258 @@
+const pdfData = [
+    {
+      chap_name:"Chapter-I: Introducation to ED",
+      pdf_name:"Introducation to ED",
+      pdf_path:"/pdfs/demo.pdf",
+      subj_name:'ed',
+      category:'assignment'
+    },
+    {
+      chap_name:"Chapter-II: Orthographic projections",
+      pdf_name:" Orthographic projections",
+      pdf_path:"/pdfs/demo.pdf",
+      subj_name:'ed',
+      category:'assignment'
+    },
+    {
+      chap_name:"Chapter-III: Projections of Solids",
+      pdf_name:"  Projections of Solids",
+      pdf_path:"/pdfs/demo.pdf",
+      subj_name:'ed',
+      category:'assignment'
+    },
+    {
+      chap_name:"Chapter-IV: Sections and Sectionals View",
+      pdf_name:"Sections and Sectionals View",
+      pdf_path:"/pdfs/demo.pdf",
+      subj_name:'ed',
+      category:'assignment'
+    },
+    {
+      chap_name:"Chapter-V:Isometric Projection",
+      pdf_name:"Isometric Projection",
+      pdf_path:"/pdfs/demo.pdf",
+      subj_name:'ed',
+      category:'assignment'
+    },
+    {
+      chap_name:"Chapter-VI:Introduction of CAD",
+      pdf_name:"Introduction of CAD",
+      pdf_path:"/pdfs/demo.pdf",
+      subj_name:'ed',
+      category:'assignment'
+    },
+  ];
+  
+  module.exports = {pdfData:pdfData};
+  
+
+  const pdfData = [
+    {
+      chap_name:"Chapter-I: DC Circuits",
+      pdf_name:" DC Circuits",
+      pdf_path:"/pdfs/demo.pdf",
+      subj_name:'beee',
+      category:'assignment'
+    },
+    {
+      chap_name:"Chapter-II: 1-Phase AC Circuits",
+      pdf_name:" 1-Phase AC Circuits",
+      pdf_path:"/pdfs/demo.pdf",
+      subj_name:'beee',
+      category:'assignment'
+    },
+    {
+      chap_name:"Chapter-III: Magnetic Circuits",
+      pdf_name:" Magnetic Circuits",
+      pdf_path:"/pdfs/demo.pdf",
+      subj_name:'beee',
+      category:'assignment'
+    },
+    {
+      chap_name:"Chapter-IV: Electrical Machines",
+      pdf_name:"Electrical Machines",
+      pdf_path:"/pdfs/demo.pdf",
+      subj_name:'beee',
+      category:'assignment'
+    },
+    {
+      chap_name:"Chapter-V: Basic Electronics",
+      pdf_name:" Basic Electronics",
+      pdf_path:"/pdfs/demo.pdf",
+      subj_name:'beee',
+      category:'assignment'
+    },
+   
+  ];
+  
+  module.exports = {pdfData:pdfData};
+  
+
+  const pdfData = [
+    {
+      chap_name:"Chapter-I:Ordinary Differential Equations 1",
+      pdf_name:" Ordinary Differential Equations 1",
+      pdf_path:"/pdfs/demo.pdf",
+      subj_name:'m-2',
+      category:'assignment'
+    },
+    {
+      chap_name:"Chapter-II: Ordinary Differential Equations 2",
+      pdf_name:" Ordinary Differential Equations 2",
+      pdf_path:"/pdfs/demo.pdf",
+      subj_name:'m-2',
+      category:'assignment'
+    },
+    {
+      chap_name:"Chapter-III: Partial Differential Equations ",
+      pdf_name:"  Partial Differential Equations",
+      pdf_path:"/pdfs/demo.pdf",
+      subj_name:'m-2',
+      category:'assignment'
+    },
+    {
+      chap_name:"Chapter-IV: Functions of Complex Variable",
+      pdf_name:"  Functions of Complex Variable",
+      pdf_path:"/pdfs/demo.pdf",
+      subj_name:'m-2',
+      category:'assignment'
+    },
+    {
+      chap_name:"Chapter-V:  Vector Calculus",
+      pdf_name:" Vector Calculus",
+      pdf_path:"/pdfs/demo.pdf",
+      subj_name:'m-2',
+      category:'assignment'
+    },
+   
+  ];
+  
+  module.exports = {pdfData:pdfData};
+  
+
+  
+  const pdfData = [
+    {
+      chap_name:"Chapter-I:Wave nature of particles and the Schrodinger equation",
+      pdf_name:" Wave nature of particles and the Schrodinger equation",
+      pdf_path:"/pdfs/demo.pdf",
+      subj_name:'phy',
+      category:'assignment'
+    },
+    {
+      chap_name:"Chapter-II: Wave Optics",
+      pdf_name:"Wave Optics",
+      pdf_path:"/pdfs/demo.pdf",
+      subj_name:'phy',
+      category:'assignment'
+    },
+    {
+      chap_name:"Chapter-III: Introducation to Solids",
+      pdf_name:" Introducation to Solids",
+      pdf_path:"/pdfs/demo.pdf",
+      subj_name:'phy',
+      category:'assignment'
+    },
+    {
+      chap_name:"Chapter-IV:  Lasers",
+      pdf_name:"   Lasers",
+      pdf_path:"/pdfs/demo.pdf",
+      subj_name:'phy',
+      category:'assignment'
+    },
+    {
+      chap_name:"Chapter-V: Electrostatics in vacuum,", 
+      pdf_name:"Electrostatics in vacuum,",
+      pdf_path:"/pdfs/demo.pdf",
+      subj_name:'phy',
+      category:'assignment'
+    },
+    
+  ];
+  
+  module.exports = {pdfData:pdfData};
+  
+
+  const pdfData = [
+    {
+      chap_name:"Chapter-I: Identifying Common errors in writing",
+      pdf_name:"  Identifying Common errors in writing",
+      pdf_path:"/pdfs/demo.pdf",
+      subj_name:'eng',
+      category:'assignment'
+    },
+    {
+      chap_name:"Chapter-II:  Vocabulary building and Comprehension",
+      pdf_name:" Vocabulary building and Comprehension",
+      pdf_path:"/pdfs/demo.pdf",
+      subj_name:'eng',
+      category:'assignment'
+    },
+    {
+      chap_name:"Chapter-III: Communication",
+      pdf_name:" Vocabulary and Comprehension",
+      pdf_path:"/pdfs/demo.pdf",
+      subj_name:'eng',
+      category:'assignment'
+    },
+    {
+      chap_name:"Chapter-IV:  Developing Writing Skills",
+      pdf_name:"  Developing Writing Skills",
+      pdf_path:"/pdfs/demo.pdf",
+      subj_name:'eng',
+      category:'assignment'
+    },
+    {
+      chap_name:"Chapter-V: Business Correspondence", 
+      pdf_name:" Business Correspondence",
+      pdf_path:"/pdfs/demo.pdf",
+      subj_name:'eng',
+      category:'assignment'
+    },
+   
+  ];
+  
+  module.exports = {pdfData:pdfData};
+  
+
+  const pdfData = [
+    {
+      chap_name:"Chapter-I: Calculus 1",
+      pdf_name:"Calculus 1",
+      pdf_path:"/pdfs/demo.pdf",
+      subj_name:'m-1',
+      category:'assignment'
+    },
+    {
+      chap_name:"Chapter-II: Calculus II",
+      pdf_name:" Calculus II",
+      pdf_path:"/pdfs/demo.pdf",
+      subj_name:'m-1',
+      category:'assignment'
+    },
+    {
+      chap_name:"Chapter-III: Sequence and Series",
+      pdf_name:" Sequence and Series",
+      pdf_path:"/pdfs/demo.pdf",
+      subj_name:'m-1',
+      category:'assignment'
+    },
+    {
+      chap_name:"Chapter-IV:  Vector Spaces",
+      pdf_name:" Vector Spaces",
+      pdf_path:"/pdfs/demo.pdf",
+      subj_name:'m-1',
+      category:'assignment'
+    },
+    {
+      chap_name:"Chapter-V: Matrices", 
+      pdf_name:" Matrices",
+      pdf_path:"/pdfs/demo.pdf",
+      subj_name:'m-1',
+      category:'assignment'
+    },
+    
+  ];
+  
+  module.exports = {pdfData:pdfData};
+  
